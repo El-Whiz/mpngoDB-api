fe79aafb88b4d34b4281073155a20b65e55cb428
Whizz <joshuaogunsemore7@gmail.com>
2026-02-12 10:54:28 +0100

    Third commit: Assignment

---
+++ BETECHIFIED/intro.js
@@ -0,0 +1,7 @@
+const my_name = "Toluwalase";
+const age = 24;
+const fav_prog_lang = "Java";
+
+console.log("Name: " + my_name);
+console.log("Age: " + age);
+console.log("Favourite programming language: " + fav_prog_lang);
